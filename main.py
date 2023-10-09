41c6a085ac30d803933ed545b1be2d34d1a1a373
henriquedpalhares <henrique.d.palhares@gmail.com>
2023-10-09 19:42:33 -0300

    ainda ta com bug porém tenho acesso

--- main.py
+++ main.py
@@ -8,20 +8,22 @@ from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
 
-SCOPES = ["https://www.googleapis.com/auth/spreadsheeets"]
+
+
+SCOPES = ['https://www.googleapis.com/auth/spreadsheets']
 
 SPREADSHEET_ID = "1nJxTKRuQQFKCuoFTfx4fExLIM6XIlKxISnbGrIP9zY4"
 
 def main():
     creds  = None
-    if os.path.exists('token.json'):
-        creds = Credentials.from_authorized_user_file('token.json', SCOPES)
+    if os.path.exists("token.json"):
+        creds = Credentials.from_authorized_user_file("token.json", SCOPES)
     if not creds or not creds.valid:
         if creds and creds.expired and creds.refresh_token:
             creds.refresh(Request())
         else:
             flow = InstalledAppFlow.from_client_secrets_file(
-                'credentials.json', SCOPES)
+                "credentials.json", SCOPES)
             creds = flow.run_local_server(port=0)
         with open("token.json","w") as token:
             token.write(creds.to_json())
@@ -30,12 +32,12 @@ def main():
         service = build("sheets","v4", credentials=creds)
         sheets = service.spreadsheets()
          
-        result = sheets.values().get(SPREADSHEET_ID=SPREADSHEET_ID, range="Sheet1!A1:C6").execute()
+        result = sheets.values().get(spreadsheetId=SPREADSHEET_ID, range="Sheet1!A1:C6").execute()
         
         values = result.get("values",[])
 
         for row in values:
-            print(values)
+            print(row)
 
     except HttpError as err:
         print(err)
